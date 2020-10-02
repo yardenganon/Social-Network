4f4ab00869951da58ea40fde3119e4805e2edb0f
Yarden Ganon <yardenganon@gmail.com>
2020-10-02 21:39:08 +0300

    Add/Remove experience/education/account

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -1,6 +1,12 @@
 import axios from 'axios'
 import { setAlert } from './alert'
-import { GET_PROFILE, PROFILE_ERROR } from './types'
+import {
+  GET_PROFILE,
+  PROFILE_ERROR,
+  UPDATE_PROFILE,
+  ACOUNT_DELETED,
+  CLEAR_PROFILE,
+} from './types'
 
 // Get current users profile
 
@@ -16,5 +22,164 @@ export const getCurrentProfile = () => async (dispatch) => {
       type: PROFILE_ERROR,
       payload: { msg: err.response.statusText, status: err.response.status },
     })
+  }
+}
+
+// Create or update a profile
+export const createProfile = (formData, history, edit = false) => async (
+  dispatch
+) => {
+  try {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }
+
+    const res = await axios.post('/api/profile', formData, config)
+
+    dispatch({
+      type: GET_PROFILE,
+      payload: res.data,
+    })
+
+    dispatch(setAlert(edit ? 'Profile Updated' : 'Profile Created'), 'success')
+
+    if (!edit) {
+      history.push('/dashboard')
+    }
+  } catch (err) {
+    const errors = err.response.data.errors
+    if (errors) {
+      errors.forEach((error) => dispatch(setAlert(error.msg, 'danger')))
+    }
+
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    })
+  }
+}
+
+// Add Experience
+export const addExperience = (formData, history) => async (dispatch) => {
+  try {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }
+
+    const res = await axios.put('/api/profile/experience', formData, config)
+
+    dispatch({
+      type: UPDATE_PROFILE,
+      payload: res.data,
+    })
+
+    dispatch(setAlert('Experience Added'), 'success')
+
+    history.push('/dashboard')
+  } catch (err) {
+    const errors = err.response.data.errors
+    if (errors) {
+      errors.forEach((error) => dispatch(setAlert(error.msg, 'danger')))
+    }
+
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    })
+  }
+}
+
+// Add Education
+export const addEducation = (formData, history) => async (dispatch) => {
+  try {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }
+
+    const res = await axios.put('/api/profile/education', formData, config)
+
+    dispatch({
+      type: UPDATE_PROFILE,
+      payload: res.data,
+    })
+
+    dispatch(setAlert('Education Added'), 'success')
+
+    history.push('/dashboard')
+  } catch (err) {
+    const errors = err.response.data.errors
+    if (errors) {
+      errors.forEach((error) => dispatch(setAlert(error.msg, 'danger')))
+    }
+
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    })
+  }
+}
+
+// Delete experience
+export const deleteExperience = (id) => async (dispatch) => {
+  try {
+    const res = await axios.delete(`/api/profile/experience/${id}`)
+
+    dispatch({
+      type: UPDATE_PROFILE,
+      payload: res.data,
+    })
+    dispatch(setAlert('Experience Removed'), 'success')
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    })
+  }
+}
+
+// Delete education
+export const deleteEducation = (id) => async (dispatch) => {
+  try {
+    const res = await axios.delete(`/api/profile/education/${id}`)
+
+    dispatch({
+      type: UPDATE_PROFILE,
+      payload: res.data,
+    })
+    dispatch(setAlert('Education Removed'), 'success')
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    })
+  }
+}
+
+// Delete account and profile
+export const deleteAccount = () => async (dispatch) => {
+  if (window.confirm('Are you sure? This can NOT be undone!')) {
+    try {
+      const res = await axios.delete(`/api/profile`)
+
+      dispatch({
+        type: CLEAR_PROFILE,
+      })
+      dispatch({
+        type: ACOUNT_DELETED,
+      })
+
+      dispatch(setAlert('Your account has been permanantly deleted'))
+    } catch (err) {
+      dispatch({
+        type: PROFILE_ERROR,
+        payload: { msg: err.response.statusText, status: err.response.status },
+      })
+    }
   }
 }
--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -3,10 +3,14 @@ import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import Spinner from '../layout/Spinner'
-import { getCurrentProfile } from '../../actions/profile'
+import DashboardActions from './DashboardActions'
+import Experience from './Experience'
+import Education from './Education'
+import { deleteAccount, getCurrentProfile } from '../../actions/profile'
 
 const Dashboard = ({
   getCurrentProfile,
+  deleteAccount,
   auth: { user },
   profile: { profile, loading },
 }) => {
@@ -22,7 +26,17 @@ const Dashboard = ({
         <i className='fas fa-user'></i>Welcome {user && user.name}
       </p>
       {profile !== null ? (
-        <Fragment>has</Fragment>
+        <Fragment>
+          <DashboardActions />
+          <Experience experience={profile.experience} />
+          <Education education={profile.education} />
+
+          <div className='my-2'>
+            <button className='btn btn-danger' onClick={() => deleteAccount()}>
+              <i className='fas fa-user-minus'>Delete My Account</i>
+            </button>
+          </div>
+        </Fragment>
       ) : (
         <Fragment>
           <p>You have not yet setup a profile, please add some info</p>
@@ -37,6 +51,7 @@ const Dashboard = ({
 
 Dashboard.propTypes = {
   getCurrentProfile: PropTypes.func.isRequired,
+  deleteAccount: PropTypes.func.isRequired,
   auth: PropTypes.object.isRequired,
   profile: PropTypes.object.isRequired,
 }
@@ -45,4 +60,6 @@ const mapStateToProps = (state) => ({
   auth: state.auth,
   profile: state.profile,
 })
-export default connect(mapStateToProps, { getCurrentProfile })(Dashboard)
+export default connect(mapStateToProps, { getCurrentProfile, deleteAccount })(
+  Dashboard
+)
