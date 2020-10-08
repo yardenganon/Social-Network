a39c1b5522729beb96f40a650a9a8198729ff902
Yarden Ganon <yardenganon@gmail.com>
2020-10-08 17:45:13 +0300

    Fixes and Not Found page

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -20,10 +20,10 @@ export const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
         </Link>
       </li>
       <li>
-        <a onClick={logout} href='#!'>
+        <Link onClick={logout} to='/login'>
           <i className='fas fa-sign-out-alt'></i>{' '}
           <span className='hide-sm'>Logout</span>
-        </a>
+        </Link>
       </li>
     </ul>
   );
--- client/src/components/profile-form/EditProfile.js
+++ client/src/components/profile-form/EditProfile.js
@@ -38,7 +38,7 @@ const EditProfile = ({
       status: loading || !profile.status ? '' : profile.status,
       skills: loading || !profile.skills ? '' : profile.skills.join(','),
       githubusername:
-        loading || !profile.githubusername ? '' : profile.guthubusername,
+        loading || !profile.githubusername ? '' : profile.githubusername,
       bio: loading || !profile.bio ? '' : profile.bio,
       twitter: loading || !profile.social ? '' : profile.social.twitter,
       facebook: loading || !profile.social ? '' : profile.social.facebook,
