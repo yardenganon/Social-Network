f5340895c2cd01348f1a1ab1933344253abc48d4
Yarden Ganon <yardenganon@gmail.com>
2020-10-08 16:04:11 +0300

    End of project

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -3,12 +3,27 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getPosts } from '../../actions/post';
 import Spinner from '../layout/Spinner';
+import PostItem from './PostItem';
+import PostForm from './PostForm';
 
-const Posts = ({ getPosts, post: { post, loading } }) => {
+const Posts = ({ getPosts, post: { posts, loading } }) => {
   useEffect(() => {
     getPosts();
   }, [getPosts]);
-  return <div></div>;
+  return loading ? (
+    <Spinner />
+  ) : (
+    <Fragment>
+      <h1 className='large text-primary'>Posts</h1>
+      <p className='lead'>
+        <i className='fas fa-user'></i> Welcome to the community
+      </p>
+      <PostForm />
+      <div className='posts'>
+        {posts && posts.map((post) => <PostItem key={post._id} post={post} />)}
+      </div>
+    </Fragment>
+  );
 };
 
 Posts.propTypes = {
